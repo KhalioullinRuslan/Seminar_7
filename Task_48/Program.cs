8a0e462a2982de3ad8ab4fc4123cbbe711cc7b63
unknown <xarus82@mail.ru>
2022-12-12 22:48:00 +0300

    Задача 48

---
+++ Task_48/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void ShowArray( int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateRandomArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+           array[i,j] = i + j;
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+ 
+//получить от пользывателя M  и N
+
+int m = EnterNumber("введите значение m: ");
+
+int n = EnterNumber("введите значение n: ");
+
+//создать двухмерный массив и заполняем
+
+int[,] matrix = CreateRandomArray(m, n);
+
+// вывести
+
+ShowArray(matrix);
